971c7c1801a24d7d33ce67abc00d9290127ed2df
Augusto Bidart <abidart@ml.com>
2020-12-16 19:56:47 -0300

    Subo app

---
+++ site/app.js
@@ -0,0 +1,18 @@
+const path = require('path');
+
+const express = require('express');
+
+const app = express();
+
+const PublicPath = path.resolve(__dirname, './public');
+app.use(express.static(PublicPath));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/index.html'));
+});
+
+app.listen(3000, () => { 
+    console.log('-----------------------------------------------')
+    console.log('Todo salio OK! Ya podés ir a http://localhost:3000');
+    console.log('-----------------------------------------------')
+});

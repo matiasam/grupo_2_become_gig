2f995284b91cb720bb8b1835a2ba692166701eee
Augusto Bidart <abidart@ml.com>
2021-01-31 18:19:29 -0300

    subo files

--- products/app.js
+++ products/app.js
@@ -4,7 +4,6 @@ const express = require('express');
 
 const app = express();
 
-// set the view engine to ejs
 app.set('view engine', 'ejs');
 
 const PublicPath = path.resolve(__dirname, './public');

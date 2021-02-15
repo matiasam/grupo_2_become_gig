4519fb7bc72518c583369c98b03156750f742c2c
Augusto Bidart <abidart@ml.com>
2021-02-15 15:39:44 -0300

    subo

--- site/app.js
+++ site/app.js
@@ -1,25 +1,17 @@
 // Modulos
 const express = require('express');
 const app = express();
-const methodOverride = require('method-override');
 
 // Configuración
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
-app.set('views', 'src/views');
-
-// Formularios
-app.use(express.urlencoded({ extended: false }));
-app.use(methodOverride('_method'));
+app.set('views', './views');
 
 // Rutas
 const mainRouter = require('./routes/mainRouter');
-const usersRouter = require('./routes/usersRouter');
-
 app.use('/', mainRouter);
-app.use('/users', usersRouter);
 
 // Iniciamos el servidor
 app.listen(3000, () => { console.log('Servidor escuchando en el puerto 3000') });
 
-//chequeo
+//comentario

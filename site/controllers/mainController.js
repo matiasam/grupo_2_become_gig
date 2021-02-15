230ea2dde37470584bc26ed142317aa7e0e51308
Augusto Bidart <abidart@ml.com>
2021-02-15 16:11:22 -0300

    subo

--- site/controllers/mainController.js
+++ site/controllers/mainController.js
@@ -1,15 +1,8 @@
-const { request } = require("express");
-
-// const users = require('../database/users');
+const users = require('../database/users');
 
 module.exports = {
     index: (req, res) => {
-        res.render('index', { 
-            title: 'Listado de usuarios', 
-            users : [
-                { first_name: 'Juan' } , { first_name: 'María' }
-            ]
-        });
+        res.render('index', {users});
     },
     detail: (req, res) => {
         res.render('detail');

9277d25bace93c8bb9c2201d7c934bc412c4851b
Matias Melian <Matias Melian>
2021-03-15 19:54:20 -0300

    test

--- site/routes/mainRouter.js
+++ site/routes/mainRouter.js
@@ -5,7 +5,6 @@ const controller = require('../controllers/mainController');
 router.get('/', controller.index);
 router.get('/detail', controller.detail);
 router.get('/contacto', controller.contact);
-router.get('/:id', controller.index);
-
+router.get('/:id', controller.detail)
 
 module.exports = router;
